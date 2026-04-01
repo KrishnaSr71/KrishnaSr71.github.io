3468765f3f0dd4fe171a341784bc52320826175e
Krishna <ksripathi19@gmail.com>
2026-03-31 23:39:57 -0400

    Build and config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -153,6 +153,7 @@ function vitePluginManusDebugCollector(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector()];
 
 export default defineConfig({
+  base: '/krishna-eportfolio/',
   plugins,
   resolve: {
     alias: {
